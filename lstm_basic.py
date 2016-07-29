2a4a9b6b734273a683b122debd4835ec6c47abbd
Guy Emerson <gete2@cam.ac.uk>
2016-07-29 16:14:25 +0100

    Use dynamic_rnn

--- lstm_basic.py
+++ lstm_basic.py
@@ -6,68 +6,78 @@
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.models.rnn import rnn, rnn_cell
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-learning_rate = 0.001
-epochs = 10
-batch_size = 128
-input_size = 28
-num_steps = 28
-num_hidden = 128 
-num_classes = 10 
+LEARNING_RATE = 0.001
+EPOCHS = 10
+BATCH_SIZE = 128
+INPUT_SIZE = 49
+NUM_STEPS = 16
+NUM_HIDDEN = 100
+NUM_CLASSES = 10
 
-def my_rnn(x, weights, biases):   # an RNN cell with a weight matrix applied to the outputs
+def my_rnn(x, weights, biases):
+    """
+    Build an RNN with a weight matrix applied to the outputs
+    :param x: input tensor with shape [BATCH_SIZE, NUM_STEPS, INPUT_SIZE]
+    :param weights: weight matrix with shape [NUM_HIDDEN, NUM_CLASSES]
+    :param biases: bias weight matrix with shape [NUM_STEPS, NUM_CLASSES]
+    """
+    cell = tf.nn.rnn_cell.BasicLSTMCell(NUM_HIDDEN, state_is_tuple=True)
 
-    x = tf.transpose(x, [1, 0, 2])   # [batch_size, num_steps, input_size] => [num_steps, batch_size, input_size]
-    x = tf.reshape(x, [-1, input_size])   # [num_steps * batch_size, input_size]
-    x = tf.split(0, num_steps, x)    # list of num_steps tensors, each of shape [batch_size, input_size]
-
-    cell = rnn_cell.BasicLSTMCell(num_hidden)
-
-    outputs, states = rnn.rnn(cell, x, dtype=tf.float32)  # outputs = list of num_steps tensors, each of shape [batch_size, num_hidden]
-
-    outputs = tf.pack(outputs) # outputs = tensor of shape [num_steps, batch_size, num_hidden]
-    outputs = tf.transpose(outputs, [1, 0, 2])  # outputs = tensor of shape [batch_size, num_steps, num_hidden]
-    outputs = tf.split(0, batch_size, outputs)
-
-    batch_out = []
-    for z in outputs:
-        batch_out.append(tf.matmul(tf.squeeze(z), weights) + biases)
-
-    return tf.pack(batch_out)     # [batch_size, num_steps, num_classes]
-
-
-x = tf.placeholder(tf.float32, [batch_size, num_steps, input_size])
-y = tf.placeholder(tf.float32, [batch_size, num_classes])
-
-W = tf.Variable(tf.zeros([num_hidden, num_classes]))
-b = tf.Variable(tf.zeros([num_steps, num_classes]))
+    outputs, states = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32) 
+    # outputs is a tensor with shape [BATCH_SIZE, NUM_STEPS, NUM_HIDDEN]
     
-pred = my_rnn(x, W, b)   # [batch_size, num_steps, num_classes]
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf.reshape(pred, [batch_size, num_steps * num_classes]),   tf.tile(y, [1,num_steps])     ))  / batch_size    # not 100% sure the correct dimensions are getting matched up here
+    # Tensorflow doesn't currently support tensor contraction (I mean, seriously...)
+    # So we have to convert to matrices and then convert back
+    matrix_outputs = tf.reshape(outputs, [-1, NUM_HIDDEN])
+    matrix_predictions = tf.matmul(matrix_outputs, W)
+    predictions = tf.reshape(matrix_predictions, [BATCH_SIZE, NUM_STEPS, NUM_CLASSES])
+    
+    # Tensorflow *does* support broadcasting
+    predictions += b
+    
+    return predictions
 
+x = tf.placeholder(tf.float32, [BATCH_SIZE, NUM_STEPS, INPUT_SIZE])
+y = tf.placeholder(tf.float32, [BATCH_SIZE, NUM_CLASSES])
 
-correct_pred = tf.equal(tf.argmax(tf.reshape(pred, [batch_size, num_steps * num_classes]), 1), tf.argmax(tf.tile(y, [1,14]), 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+W = tf.Variable(tf.zeros([NUM_HIDDEN, NUM_CLASSES]))
+b = tf.Variable(tf.zeros([NUM_STEPS, NUM_CLASSES]))
 
-optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+pred = my_rnn(x, W, b)
+
+# We're using a single MNIST image as a "sequence", so we need to broadcast the classes
+broad_y = tf.tile(tf.reshape(y, [BATCH_SIZE, 1, NUM_CLASSES]), [1, NUM_STEPS, 1])
+
+# To calculate the softmax cross-entropy, Tensorflow expects matrices,
+# where each row is a distribution over classes
+matrix_pred = tf.reshape(pred, [-1, NUM_CLASSES])
+matrix_y = tf.reshape(broad_y, [-1, NUM_CLASSES])
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(matrix_pred, matrix_y)
+# Find the average
+cost = tf.reduce_mean(cross_entropy)
+
+# Also caculate 1-best prediction accuracy
+index_pred = tf.argmax(matrix_pred, 1)
+index_y = tf.argmax(matrix_y, 1)
+correct = tf.equal(index_pred, index_y)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+# Train the model
+optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
 
 init = tf.initialize_all_variables()  # Must be done AFTER introducing the optimizer (see http://stackoverflow.com/questions/33788989/tensorflow-using-adam-optimizer)
 
 with tf.Session() as sess:
 
     sess.run(init)
-    step = 1
-    while step < epochs:
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        batch_x = batch_x.reshape((batch_size, num_steps, input_size))
+    for step in range(EPOCHS):
+        batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
+        batch_x = batch_x.reshape((BATCH_SIZE, NUM_STEPS, INPUT_SIZE))
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
         acc = sess.run(accuracy, feed_dict = {x: batch_x, y: batch_y})
         loss = sess.run(cost, feed_dict = {x: batch_x, y: batch_y})
-        print "Epoch", str(step * batch_size), "Minibatch loss", "{:.6f}".format(loss), "Training acc", "{:.5f}".format(acc)
-        step += 1
-
-
+        print("Epoch {}\nMinibatch loss {:.6f}\nTraining acc {:.5f}".format(step+1, loss, acc))
