33312344f85b5bbb11d9f49ea8fd66322506b6fa
Kevin Heffernan <berkleepercussion@gmail.com>
2016-08-30 15:31:45 +0100

    new instructions added to access tokens

--- conll_demo.py
+++ conll_demo.py
@@ -1,7 +1,6 @@
 # HOW TO USE CONLL PARSER
 
 from conll import *
-from matrix_gen import *
 
 corpus_dir = "/anfs/bigdisc/kh562/Corpora/conll-2011/"
 conll_reader = ConllCorpusReader(corpus_dir)
@@ -10,18 +9,14 @@ train_conll_docs = conll_reader.get_conll_docs("train")
 test_conll_docs = conll_reader.get_conll_docs("test")
 
 for conll_doc in train_conll_docs:
-    #get the s_matrix for each document
-    matrix=get_s_matrix(conll_doc)
-    # this contains all the sentences for each document and can be used to create T in matrix
-    sents = conll_doc.get_sents()
+    # list of all tokens in document
+    document_tokens = conll_doc.get_document_tokens()
     # the coref chain consists of a list of coreferent mentions for each key chain id
-    coref_chain = conll_doc.get_coref_chain()
+    coref_chain = conll_doc.coref_chain
     for chain_id in coref_chain:
         mentions = coref_chain[chain_id]
         for mention in mentions:
-            chain_id = mention.get_chain_id()
-            # this gives the words for each mention in document (M in matrix)
-            tokens = mention.get_tokens()
-            # the exact index of each mention is below and can be found in "sents"
-            # (probably not needed given architecture we're using)
-            (sent_index,start_index,end_index) = mention.get_indices()
+            chain_id = mention.chain_id
+            index = mention.get_document_index(conll_doc)
+            # this gives the tokens for each mention in document (M in matrix)
+            tokens = document_tokens[index[0]:index[1]+1]
