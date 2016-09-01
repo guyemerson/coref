98afe585948a43a91929d94987d069cc587301f3
OlesyaR <olesya796@gmail.com>
2016-09-01 13:12:07 +0100

    modified S_matrix
    modified S_matrix: 
    1) the columns are all tokens in the document
    2) does not preprocess mentions with regards to the punctuation marks and upper case
    3) takes the list of tokens directly from conll.py

--- matrix_gen.py
+++ matrix_gen.py
@@ -11,28 +11,22 @@ import string
 
 
 class matrix_generator:
-    def get_s_matrix(self, doc):
-        T_matrix=[]
-        S_matrix=[]
-        sents = doc.get_sents()
-        # generate the T matrix
-        for sent in sents:
-            words = [w for w in sent if not w in set(string.punctuation)] #have to discuss whether we need to lowercase and preprocess words
-            for token in words:
-                if not token in T_matrix:
-                    T_matrix.append(token)
-        # the coref chain consists of a list of coreferent mentions for each key chain id
-        coref_chain = doc.get_coref_chain()
-        #generate the S_matrix
-        for chain_id in coref_chain:
-            mentions = coref_chain[chain_id]
-            for mention in mentions:
-                row=list(T_matrix)
-                tokens = [w for w in mention.get_tokens() if not w in set(string.punctuation)]
-                for i, token in enumerate(row):
-                    if not token in tokens:
-                        row[i]=0
-                    else:
-                        row[i]=1/len(tokens)
-                S_matrix.append(row)
-        return(S_matrix)
+    def get_s_matrix(doc):
+    # generate the T matrix and initialize S_matrix
+    T_matrix=doc.get_document_tokens()
+    S_matrix=[]
+    # the coref chain consists of a list of coreferent mentions for each key chain id
+    coref_chain = doc.coref_chain
+    #generate the S_matrix
+    for chain_id in coref_chain:
+        mentions = coref_chain[chain_id]
+        for mention in mentions:
+            #initialize the matrix row
+            row=[0]*len(T_matrix)
+            chain_id = mention.chain_id
+            index = mention.get_document_index(doc)
+            # assign values to the mention tokens in the tokens vector
+            for i in range(index[0],(index[1]+1)):
+                row[i]=1/(index[1]+1-index[0])
+            S_matrix.append(row)
+    return(S_matrix)
