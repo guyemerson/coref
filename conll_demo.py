b1e36243b0195dea9633dd505fd153632f729ce0
Kevin Heffernan <kh562@cl.cam.ac.uk>
2017-03-26 17:49:22 +0100

    updated conll.py usage with dev corpus

--- conll_demo.py
+++ conll_demo.py
@@ -2,11 +2,11 @@
 
 from conll import ConllCorpusReader
 
-corpus_dir = "/anfs/bigdisc/kh562/Corpora/conll-2011/"
+corpus_dir = "/anfs/bigdisc/kh562/Corpora/conll-2012/v4/data/"
 conll_reader = ConllCorpusReader(corpus_dir)
 conll_reader.parse_corpus()
 train_conll_docs = conll_reader.get_conll_docs("train")
-test_conll_docs = conll_reader.get_conll_docs("test")
+dev_conll_docs = conll_reader.get_conll_docs("development")
 
 for conll_doc in train_conll_docs:
     # list of all tokens in document
@@ -15,8 +15,11 @@ for conll_doc in train_conll_docs:
     coref_chain = conll_doc.coref_chain
     for chain_id in coref_chain:
         mentions = coref_chain[chain_id]
+        if len(mentions)==217:
+            print document_tokens
         for mention in mentions:
             chain_id = mention.chain_id
             index = mention.get_document_index(conll_doc)
             # this gives the tokens for each mention in document (M in matrix)
             tokens = document_tokens[index[0]:index[1]+1]
+
