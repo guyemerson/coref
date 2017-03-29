735bb8c29ed203661afafb3ee1806164755094af
Laura Rimell <laura.rimell@cl.cam.ac.uk>
2017-03-29 16:20:52 +0100

    Calculate avg metrics, command line options for params

--- lstm_basic.py
+++ lstm_basic.py
@@ -4,25 +4,39 @@
 
 import numpy as np
 import tensorflow as tf
+import argparse
+from collections import defaultdict
 
 from conll import ConllCorpusReader
 from matrix_gen import get_s_matrix, coref_matrix
 from evaluation import get_evaluation
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--print_document_scores", help="Print metrics per document during training", action="store_true")
+parser.add_argument("--print_minibatch_loss", help="Print minibatch (document) loss during training", action="store_true")
+parser.add_argument("--print_dev_loss", help="Print minibatch (document) loss during evaluation on the dev set", action="store_true")
+parser.add_argument("--epochs", help="Number of training epochs", default=20)
+parser.add_argument("--learning_rate", help="Learning rate for training", default=0.1)
+parser.add_argument("--hidden_size", help="Number of hidden units", default=600)
+parser.add_argument("--threshold", help="Threshold value for coference (between 0 and 1)", default=0.79)
+args = parser.parse_args()
+
 corpus_dir = "/anfs/bigdisc/kh562/Corpora/conll-2012/v4/data/"
 conll_reader = ConllCorpusReader(corpus_dir).parse_corpus()
+
+metrics = ['muc', 'bcub', 'ceafm', 'ceafe', 'blanc', 'lea']
 
 tf.set_random_seed(1234)
 
 ### Hyperparameters
 
-LEARNING_RATE = 0.1
-EPOCHS = 20
+LEARNING_RATE = float(args.learning_rate)
+EPOCHS = int(args.epochs)
 # TODO: input size to be set to 300 when using cached vectors (i.e. real data)
 INPUT_SIZE = 300
-NUM_HIDDEN = 600
+NUM_HIDDEN = int(args.hidden_size)
 # threshold for cosine similarity on coref matrix (C) 
-THRESHOLD=0.79
+THRESHOLD=float(args.threshold)
 
 # Currently hard-coding the batch size to be 1
 # This reduces the amount of reshaping that Tensorflow needs to do tensor contraction
@@ -93,6 +107,7 @@ with tf.Session() as sess:
     sess.run(init)
     print("Starting session")
     for step in range(EPOCHS):
+        metrics_this_epoch = defaultdict(lambda: defaultdict(list))
         for i in range(len(train_conll_docs)):
             current_dict = {x: train_docs[i], y: train_coref_matrix[i], s: train_s_matrix[i]}
             sess.run(optimizer, feed_dict=current_dict)
@@ -100,11 +115,54 @@ with tf.Session() as sess:
             loss = sess.run(error_rate, feed_dict=current_dict)
             coref_mat = sess.run(nonneg_sim, feed_dict=current_dict)
             # get evaluation of current predicted coref matrix
-            print(get_evaluation(train_conll_docs[i],coref_mat,THRESHOLD)["formatted"])
-            # print("Epoch {}\nMinibatch loss {:.6f}\nTraining acc {:.5f}".format(step+1, loss, acc))
-            print("Epoch {}\nDocument {}\nMinibatch loss {:.6f}".format(step+1, i, loss))
+            evals = get_evaluation(train_conll_docs[i],coref_mat,THRESHOLD)
+            for m in metrics:
+                metrics_this_epoch[m]['R'].append(evals[m][0])
+                metrics_this_epoch[m]['P'].append(evals[m][1])
+                metrics_this_epoch[m]['F1'].append(evals[m][2])
+            metrics_this_epoch['conll']['avg'].append(evals['avg'])
+            if args.print_document_scores:
+                print(evals["formatted"])
+            if args.print_minibatch_loss:
+                # print("Epoch {}\nMinibatch loss {:.6f}\nTraining acc {:.5f}".format(step+1, loss, acc))
+                print("Epoch {}\nDocument {}\nMinibatch loss {:.6f}".format(step+1, i, loss))
 
+
+        avg_scores = defaultdict(lambda: defaultdict(float))
+        for m in metrics_this_epoch:
+            for t in metrics_this_epoch[m]:
+                avg_scores[m][t] = sum(metrics_this_epoch[m][t]) / len(metrics_this_epoch[m][t])
+        formatted = "\tR\tP\tF1\n"
+        for m in metrics:
+            formatted += m + "\t" + format(avg_scores[m]['R'], '.2f') + "\t" +  format(avg_scores[m]['P'], '.2f') + "\t" + format(avg_scores[m]['F1'], '.2f') + "\n"
+        formatted += "\n"
+        formatted += "conll\t\t\t" + format(avg_scores['conll']['avg'], '.2f') + "\n"
+        print("AVERAGE METRICS FOR EPOCH", step+1)
+        print(formatted)
+            
+
+        print("Evaluating on dev set\n")
         for i in range(len(dev_conll_docs)):
             current_dict = {x: dev_docs[i], y: dev_coref_matrix[i], s: dev_s_matrix[i]}
             loss = sess.run(error_rate, feed_dict=current_dict)
-            print("Document {}\nLoss {:.6f}".format(i, loss))
+            if args.print_dev_loss:
+                print("Document {}\nLoss {:.6f}".format(i, loss))
+            coref_mat = sess.run(nonneg_sim, feed_dict=current_dict)
+            # get evaluation of current predicted coref matrix
+            evals = get_evaluation(dev_conll_docs[i],coref_mat,THRESHOLD)
+            for m in metrics:
+                metrics_this_epoch[m]['R'].append(evals[m][0])
+                metrics_this_epoch[m]['P'].append(evals[m][1])
+                metrics_this_epoch[m]['F1'].append(evals[m][2])
+            metrics_this_epoch['conll']['avg'].append(evals['avg'])
+        avg_scores = defaultdict(lambda: defaultdict(float))
+        for m in metrics_this_epoch:
+            for t in metrics_this_epoch[m]:
+                avg_scores[m][t] = sum(metrics_this_epoch[m][t]) / len(metrics_this_epoch[m][t])
+        formatted = "\tR\tP\tF1\n"
+        for m in metrics:
+            formatted += m + "\t" + format(avg_scores[m]['R'], '.2f') + "\t" +  format(avg_scores[m]['P'], '.2f') + "\t" + format(avg_scores[m]['F1'], '.2f') + "\n"
+        formatted += "\n"
+        formatted += "conll\t\t\t" + format(avg_scores['conll']['avg'], '.2f') + "\n"
+        print("AVERAGE METRICS ON DEV SET, EPOCH", step+1)
+        print(formatted)
