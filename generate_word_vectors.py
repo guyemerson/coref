709ecd9d946698615a6170b2bbc936835787973b
Kevin Heffernan <kh562@cl.cam.ac.uk>
2017-04-12 18:16:27 +0100

    Updated cached vectors with new binary features

--- generate_word_vectors.py
+++ generate_word_vectors.py
@@ -1,9 +1,8 @@
 """ code to generate vectors for each token in CoNLL document """
 
 import numpy as np
-from string import digits, punctuation
 import gensim
-
+from string import digits, punctuation
 from conll import ConllCorpusReader
 
 # Google's pre-trained word2vec model from GoogleNews. Note: this 
@@ -11,44 +10,75 @@ from conll import ConllCorpusReader
 model = gensim.models.KeyedVectors.load_word2vec_format('/anfs/bigdisc/kh562/Models/GoogleNews-vectors-negative300.bin.gz', binary=True)
 
 # Load CoNLL corpus
-corpus_dir = "/anfs/bigdisc/kh562/Corpora/conll-2012/v4/data/"
+corpus_dir = "/anfs/bigdisc/kh562/Corpora/conll-2012/"
 conll_reader = ConllCorpusReader(corpus_dir)
 conll_reader.parse_corpus()
 train_conll_docs = conll_reader.get_conll_docs("train")
 dev_conll_docs = conll_reader.get_conll_docs("development")
+test_conll_docs = conll_reader.get_conll_docs("test")
 
 # Translation table to convert digits to #
 digit_table = str.maketrans(digits, '#'*len(digits))
 # Translation table to remove punctuation
 punc_table = str.maketrans('', '', punctuation)
+# load upenn pos tagset
+upenn_tagset = [l.strip() for l in open("upenn_tagset.txt","r").readlines()]
 
-train_matrices=[]
-dev_matrices=[]
+# maximum number of speakers in CoNLL corpus
+MAX_SPEAKERS = 10
 
-def get_vector(token):
+train_matrices, dev_matrices, test_matrices = [],[],[]
+
+def get_token_vectors(conll_doc):
     """
-    Find the index for a token, preprocessing if necessary 
+    Find the vector for a token, preprocessing if necessary 
     """
-    # Try looking up the token directly
-    if token in model:
-        return model[token]
-    # Otherwise, try replacing digits with #
-    token = token.translate(digit_table)
-    if token in model:
-        return model[token]
-    # Otherwise, try removing punctuation
-    token = token.translate(punc_table)
-    if token in model:
-        return model[token]
-    # Otherwise, use the null token
-    return model['</s>']
+    pos_tags = conll_doc.pos_tags
+    speakers = conll_doc.speakers
+    speaker_list = sorted(list(set(speakers)))
+    # remove cases where no speaker
+    if "-" in speaker_list:
+        speaker_list.remove("-")
+    token_index = 0
+    token_vectors = []
+    for sent in conll_doc.sents:
+        for i,token in enumerate(sent):
+            # Try looking up the token directly
+            if token in model:
+                vector = model[token]
+            else:
+                # Otherwise, try replacing digits with #
+                token = token.translate(digit_table)
+                if token in model:
+                    vector = model[token]
+                else:
+                    # Otherwise, try removing punctuation
+                    token = token.translate(punc_table)
+                    if token in model:
+                        vector =  model[token]
+                    # Otherwise, use the null token
+                    else: vector = model['</s>']
+            # check if this is the last token in sent
+            vector = np.append(vector,1 if i==len(sent)-1 else 0)
+            # check if token is capitalised
+            vector = np.append(vector,1 if token.isupper() else 0)
+            # get binary pos tag feature
+            vector = np.append(vector,[0 if i!=upenn_tagset.index(pos_tags[token_index]) else 1 for i in range(len(upenn_tagset))])
+            # get binary presence of speaker n. "-" indicates no speaker
+            if speakers[token_index]=="-":
+                vector = np.append(vector,[0] * MAX_SPEAKERS)
+            else:
+                vector = np.append(vector,[0 if i!=speaker_list.index(speakers[token_index]) else 1 for i in range(MAX_SPEAKERS)])
+            token_vectors.append(vector)
+            token_index+=1
+    return token_vectors
 
 # repeat for "test_conll_docs" etc
-for conll_doc in dev_conll_docs:
+for conll_doc in train_conll_docs:
     # list of numpy arrays containing vectors for each token in document
-    token_vectors = [get_vector(token) for token in conll_doc.get_document_tokens()]
+    token_vectors = get_token_vectors(conll_doc)
     # convert list of vectors to matrix
     token_vector_matrix = np.array(token_vectors)
-    dev_matrices.append(token_vector_matrix)
+    train_matrices.append(token_vector_matrix)
 
-np.savez("/anfs/bigdisc/kh562/Corpora/conll-2012/development_docs",matrices=dev_matrices)
+np.savez("/anfs/bigdisc/kh562/Corpora/conll-2012/training_docs_new",matrices=train_matrices)
